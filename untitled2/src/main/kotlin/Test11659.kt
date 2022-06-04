1f04e217aeb7c983be23a9f910d6adb29863689e
XELPINE <xelpine@gmail.com>
2022-06-04 09:51:38 +0900

    11659 (실패)

--- untitled2/src/main/kotlin/Test11659.kt
+++ untitled2/src/main/kotlin/Test11659.kt
@@ -1,82 +1,72 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 import kotlin.collections.ArrayList
 
 fun main() {
 
-    // 구간합 문제
+    // 구간합 문제 (답은 맞는데 잘 안되네)
+    val buffer : BufferedReader = BufferedReader(InputStreamReader(System.`in`))
 
-    // 정수 하나 읽기
-    val sc: Scanner = Scanner(System.`in`)
-    val inputOriginal = sc.nextLine()  // Long
-    sc.nextLine();  // Consume newline left-over
-    print(" ")
-    var inputNumberList = sc.nextLine().split(" ").map { it.toLong() }    // 수 리스트
-    var inputNumberAddResultList = mutableListOf<Long>()    // 수 리스트의 구간합 리스트
-    var resultList = mutableListOf<Long>()
+    val inputArraySizeAndQuizSize = StringTokenizer(buffer.readLine(), " ")
 
-    val inputNumber = inputOriginal.substring(0, inputOriginal.indexOf(" ")).toInt()
-    val inputIndex = inputOriginal.substring(inputOriginal.indexOf(" ")+1, inputOriginal.length).toInt()
+    val arraySize = inputArraySizeAndQuizSize.nextToken().toInt()
+    val quizSize = inputArraySizeAndQuizSize.nextToken().toInt()
 
-    var sectorList = mutableListOf<String>()
-    var sectorStartList = mutableListOf<Int>()
-    var sectorEndList = mutableListOf<Int>()
+//    println ("arraySize : " + arraySize)
+//    println ("quizSize : " + quizSize)
 
-    // inputIndex 만큼 입력받기
-    for (index : Int in 0 until inputIndex)
-    {
-        //print(" ")
-//        sectorList.add(sc.nextLine())    // 수 리스트
-        sectorStartList.add(sc.nextInt())
-        sectorEndList.add(sc.nextInt())
-    }
+//    val inputNumberArray = StringTokenizer(buffer.readLine(), " ")
 
-//    println("inputNumber (수의 갯수) : $inputNumber")
-//    println("inputIndex (전체 처리할 횟수) : $inputIndex")
+    // 공백용
+    val temp1 = buffer.readLine().split(" ").map {  }
 
-    for (index : Int in 0 until inputNumberList.size)
-    {
-//        println("inputNumberList (수 리스트) : ${inputNumberList.get(index)}")
+    val numberArray = buffer.readLine().split(" ").map { it.toLong() }
 
-            if (index == 0)
-            {
-                inputNumberAddResultList.add(inputNumberList.get(index))
-            }
-            else
-            {
-                inputNumberAddResultList.add(inputNumberAddResultList.get(index-1) + inputNumberList.get(index))
-            }
-    }
-
-//    for (index : Int in 0 until inputNumberAddResultList.size)
+//    for (index : Int in 0 until numberArray.size)
 //    {
-//        println("inputNumberAddResultList (구간합 리스트) : ${inputNumberAddResultList.get(index)}")
+//        println ("numberArray : " + numberArray.get(index))
 //    }
 
+    var sumArray = mutableListOf<Long>()
 
-    // 최종결과값 내기
-    for (index : Int in 0 until sectorStartList.size)
+    for (index : Int in 0 until numberArray.size)
     {
-//        println("==================")
-//        println("sectorList (구간합 리스트) : ${sectorList.get(index)}")
-
-//        val start = sectorList.get(index).substring(0, sectorList.get(index).indexOf(" ")).toInt()
-//        val end = sectorList.get(index).substring(sectorList.get(index).indexOf(" ")+1, sectorList.get(index).length).toInt()
-        var result : Long = 0
-
-//        println("start (구간합 시작) : " + sectorStartList.get(index) + " / " + inputNumberAddResultList.get(sectorStartList.get(index)-1))
-//        println("end (구간합 종료) : " + sectorEndList.get(index) + " / " + inputNumberAddResultList.get(sectorEndList.get(index)-1))
-
-        if (sectorStartList.get(index) == 1)
+        if (index == 0)
         {
-            result = inputNumberAddResultList.get(sectorEndList.get(index)-1)
+            sumArray.add(index, numberArray.get(index))
         }
         else
         {
-            result = inputNumberAddResultList.get(sectorEndList.get(index)-1) - inputNumberAddResultList.get(sectorStartList.get(index)-2)
+            sumArray.add(index, sumArray.get(index-1) + numberArray.get(index))
         }
 
-//        println("result (구간합) : $result")
+    }
 
-        println(result)
+    for (index : Int in 0 until sumArray.size)
+    {
+        println ("sumArray : " + sumArray.get(index))
+    }
+
+
+    val resultArray = mutableListOf<Long>()
+
+    for (index : Int in 0 until quizSize)
+    {
+        // 공백용
+        val numberArray = buffer.readLine().split(" ").map {  }
+
+        val inputQuiz = StringTokenizer(buffer.readLine(), " ")
+
+        val start = inputQuiz.nextToken().toInt()
+        val end = inputQuiz.nextToken().toInt()
+        val result = sumArray.get(end-1) - (if (start-2 >= 0) sumArray.get(start-2) else 0)
+
+        resultArray.add(result)
+    }
+
+    for (index : Int in 0 until resultArray.size)
+    {
+        println(resultArray.get(index))
     }
 }
